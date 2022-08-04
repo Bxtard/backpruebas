7478ae9ac5c36bf1c0bb32c276fee957f36a5e6d
Bryan <ee.mxtrd@gmail.com>
2022-08-04 13:19:28 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//? 1 se importa express
+const express = require('express');
+
+//? 2 se declara
+const app = express();
+
+//? 3 se instancia una respuesta al home
+app.get('/', (req, res) =>{
+  res.send('<h1>Hola Mundo</h1>')
+});
+
+//? 4 se pone el servidor en escucha
+app.listen(3000, () => console.log('listening on port 3000!'));
